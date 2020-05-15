c7b340ca28252f192e4602b92b49b80c793eeab3
tuli <tavcoh@m.telhai.ac.il>
2020-05-15 17:57:53 +0300

    terminal

---
+++ src/Json/JsonBuilder.java
@@ -0,0 +1,16 @@
+package Json;
+
+public class JsonBuilder implements JsonValue {
+    private CharScanner c;
+    @Override
+
+
+    public JsonValue get(int i) {
+        return null;
+    }
+
+    @Override
+    public JsonValue get(String s) {
+        return null;
+    }
+}
