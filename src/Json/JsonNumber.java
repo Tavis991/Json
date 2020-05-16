8f144d2fd574de91000bf4457e055bd5435ed607
Tavis991 <45292394+Tavis991@users.noreply.github.com>
2020-05-16 16:05:28 +0300

    Add files via upload
    number methods

--- src/Json/JsonNumber.java
+++ src/Json/JsonNumber.java
@@ -1,9 +1,16 @@
 package Json;
 
 public class JsonNumber implements JsonValue {
+    Number k;
+
+    public JsonNumber(Number num) {
+        this.k=num;
+    }
+
     @Override
-    public JsonValue get(int i) {
-        return null;
+
+    public JsonValue get(int i) throws JsonQueryException {
+        throw new JsonQueryException("nonono");
     }
 
     @Override
