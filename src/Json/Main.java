df58a86ca5f0b52269a35af41cf7b4840831712a
tuli <tavcoh@m.telhai.ac.il>
2020-05-18 11:36:10 +0300

    cmd args

--- src/Json/Main.java
+++ src/Json/Main.java
@@ -8,11 +8,13 @@ public class Main {
     public static void main(String[] args) {
 
 		JsonBuilder k = null;
-		
+		if (args.length!=1){
+			System.out.println("Program requires one input parameter : <file name/full path>");
+			System.exit(1);
+		}
 		try {
 			k = new JsonBuilder(args[0]);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
