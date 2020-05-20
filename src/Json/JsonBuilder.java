6cd7b4146cbf51fda4f6bccfe1b8c7ef16f1ae1c
tuli <tavcoh@m.telhai.ac.il>
2020-05-20 12:26:04 +0300

    final halas

--- src/Json/JsonBuilder.java
+++ src/Json/JsonBuilder.java
@@ -6,91 +6,82 @@ import java.lang.Integer;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class JsonBuilder implements JsonValue{
+public class JsonBuilder implements JsonValue {
     private CharScanner cs;
     private JsonValue v;
 
-    public JsonBuilder() throws FileNotFoundException, JsonSyntaxException
-    {
-    	cs = new CharScanner(new File("example.txt"));
-        v = parseValue();
+    public JsonBuilder(String filename) throws FileNotFoundException {
+    	cs = new CharScanner(new File(filename));
+
+        try
+        {
+        	v = parseValue();
+        }
+        catch(Exception e)
+        {
+        	e.printStackTrace();
+        }
     }
 
     public JsonString parseString() throws JsonSyntaxException
     {
-        StringBuilder build = new StringBuilder(); // will build the string for Jstr
+        StringBuilder build = new StringBuilder();
         JsonString Jstr = new JsonString();
-        char current;
-        boolean inside_string = false;
-        cs.next(); // reading the first "
+        char ch1,ch2=' ';
+        cs.next();
         while(cs.hasNext())
         {
-        	current = cs.nextWithSpaces();
+        	ch1=ch2;
         	
-        	// if (current = '\') there is 2 legal options after it: " or \
-        	if(current=='\\')
-        	{
-        		if(cs.peek()=='"')
-        		{
-        			// every \" have 2 options meaning: starting or ending an inside string
-            		inside_string=!inside_string; // true->false , false -> true
-            		continue;         			
-        		}
-        		else if(cs.peek()=='\\')
-        		{	
-        			build.append(current);
-        			cs.next();
-        			continue;
-        		}
-        		
-        		// illegal option
-        		else throw new JsonSyntaxException("Error in parseString(): \\ with no context");
-        	}
-        	
-        	// if not \ and there is " after we finish the string
-        	else if (cs.peek()=='"') 
-        	{
-        		build.append(current);
-        		break;
-        	}
-        	build.append(current);     	
+        	if(cs.hasNext()) ch2=cs.next();
+        	else break;
+        	if(ch2=='"' && ch1 !='\\') break;
+        	if(ch2=='\\' && cs.peek() == '"') continue;
+            build.append(ch2);
+
         }
-        cs.next(); // reading the last "
-        if(inside_string==true) throw new JsonSyntaxException("Error in parseString(): inside string never closed");
         Jstr.setS(build.toString());
         return Jstr;
     }
         
     
-    public JsonArray parseArray() throws JsonSyntaxException
-    {
+    public JsonArray parseArray() throws JsonSyntaxException{
+
         JsonArray arr = new JsonArray();
-        cs.next(); // reading the '['
-        char ch;
-        while (cs.hasNext())
+        cs.next();
+        while (cs.hasNext() && cs.peek()!=']')
         {
-            arr.getA().add(parseValue()); // adding the next JsonValue to arr
-            ch = cs.next();
-            if(ch==']') break;
-            if(!cs.hasNext()) throw new JsonSyntaxException("Error in parseArray(): ']' missing ");
-            if(ch!=',') throw new JsonSyntaxException("Error in parseArray(): ',' or ']' is missing ");
-            else if(chCheck(cs.peek())=="nothing") 
-            	throw new JsonSyntaxException("Error in parseArray(): illegal char after ','");
+                try 
+                {
+                    arr.getA().add(parseValue());
+                    if(cs.peek()==',') cs.next();
+                }
+                catch (JsonSyntaxException e) 
+                {
+                    e.printStackTrace();
+                }
         }
+        cs.next();
         return arr;
     }
-    
-    public JsonValue parseValue() throws JsonSyntaxException
-    {
+    public JsonValue parseValue() throws JsonSyntaxException{
         String choice;
-        if (cs.hasNext())
+        if (cs.hasNext()) 
         {
-            choice = chCheck(cs.peek());
+            try
+            {
+            	choice = chCheck(cs.peek());
+            }
+            catch (JsonSyntaxException e)
+            {
+            	throw e;
+            }
+            	
             switch (choice) 
             {
                 case "ArrayStart":
                     return parseArray();
-                case "StrStart":
+                case "Str":
                 	return parseString();
                 case "AssoArrayStart":
                 	return parseObject();
@@ -107,31 +98,21 @@ public class JsonBuilder implements JsonValue{
     	JsonObject jo = new JsonObject();
     	String key;
     	JsonValue value;
-    	char ch;
-    	cs.next(); // reading the '{'
-    	
-    	while(cs.hasNext())
+    	cs.next(); 
+    	while(cs.hasNext() && cs.peek()!='}')
     	{
-    		/* Building the key */
-    		if(cs.peek()!='"') throw new JsonSyntaxException("Error in parseObject(): key is not a String");
-    		key = parseString().getS();
-    		
-    		/* Making sure value is ok, building it if yes */
-    		ch = cs.next();
-    		if(ch!=':') throw new JsonSyntaxException("Error in parseObject(): ':' is missing");
-    		else if(chCheck(cs.peek())=="nothing")
-    			throw new JsonSyntaxException("Error in parseObject(): illegal char after ':' ");
-    		value = parseValue();
-    		
-    		jo.getO().put(key, value); // put (key:value) in JsonObject's map
-    		
-    		/* reading next char, should be ',' or '}' , with no additional chars, otherwise throws exception */
-    		ch = cs.next(); 
-    		if(ch=='}') break;
-    		if(!cs.hasNext()) throw new JsonSyntaxException("Error in parseObject(): '}' is missing");
-    		if(ch!=',') throw new JsonSyntaxException("Error in parseObject(): ',' or '}' is missing");
-    		else if(chCheck(cs.peek())=="nothing")
-    			throw new JsonSyntaxException("Error in parseObject(): illegal char after ',' ");
+    		try
+    		{
+    			key = parseString().getS();
+    			cs.next();
+    			value = parseValue();
+    			cs.next();
+    			jo.getO().put(key, value);
+    		}
+    		catch(JsonSyntaxException e)
+    		{
+    			throw e;
+    		}
     	}
     	return jo;
     }
@@ -144,17 +125,38 @@ public class JsonBuilder implements JsonValue{
         if (ch == '[') {
             return "ArrayStart";
         }
+        if (ch == ']') {
+            return "ArrayEnd";
+        }
         if (ch == '{') {
             return "AssoArrayStart";
         }
+        if (ch == '}') {
+            return "AssoArrayEnd";
+        }
         if (ch == '"') {
-            return "StrStart";
+            return "Str";
         }
         return "nothing";
+       // throw new JsonSyntaxException("unknown character. Error in chCheck()");
+    }
+    //Json parsenumber helper functions
+    public String strtCheck(char ch) throws JsonSyntaxException {
+        if (ch == '[') {
+            return "ArrayStart";
+        }
+        if (isDigit(ch) || ch == '-'){
+            return "Num";
+        }
+        if (ch == '"') {
+            return "Str";
+        }
+        if (ch == '{') {
+            return "AssoArrayStart";
+        }
+        throw new  JsonSyntaxException("no possible");
     }
     
-    
-    //parseNumber() helper functions
     public boolean numCheck(char ch){
 
         if (isDigit(ch) || isE(ch)|| isOp(ch)) return true;
@@ -174,81 +176,73 @@ public JsonNumber parseNumber() throws JsonSyntaxException{
     cnPs=cs.next();
 
     //check special initial conditions
-    switch (cnPs) 
-    {
-        case ('-'): 
-        {
+    switch (cnPs) {
+        case ('-'): {
             flagMinus++;
             bild.append(cnPs);
-            if (cs.hasNext() && numCheck(cs.peek())) cnPs = cs.next();
+            if (cs.hasNext() && numCheck(cs.peek())) { cnPs = cs.next(); }
+            break;
         }
-        case ('0'): 
-        {
-            if (cs.hasNext() && !(cs.peek() == '.'))
+        case ('0'): {
+            if (cs.hasNext() && !(cs.peek() == '.')) {
                 throw new JsonSyntaxException("Json whole numbers cannot begin with zero");
-
+          }
         }
     }
     //main loop, reads chars and confirms Json number syntax, throws exception else
-    while (cs.hasNext())
-    {
-        if(numCheck(cs.peek()))
-        {	
-            cnNs=cs.next();
-            if (isOp(cnPs)) 
-            {
-            	if ((isDigit(cnNs)&& isE(bild.charAt(bild.length()-1))))
-                {
-                    if (cnPs=='-'){ if(++flagMinus>2) throw new JsonSyntaxException("notminus"); }
-                    else if(++flagPlus>2) throw new JsonSyntaxException("notplus");
-                    bild.append(cnPs);
-                    cnPs=cnNs;
-                    continue;
-                }
-                else throw new JsonSyntaxException("not+-");
-            }
-            if (isE(cnPs)) 
-            {
-                 if (isDigit(cnNs) || isOp(cnNs)) 
-                 {
-                      if(++flagE>1) throw new JsonSyntaxException("notE");
-                      bild.append(cnPs);
-                      cnPs=cnNs;
-                      continue;
+    while (cs.hasNext()){
+        if(numCheck(cs.peek())){
+            try {
+                    cnNs=cs.next();
+                    if (isOp(cnPs)) {
+                        if ((isDigit(cnNs)&& isE(bild.charAt(bild.length()-1))))
+                        {
+                            if (cnPs=='-'){ if(++flagMinus>2) throw new JsonSyntaxException("notminus"); }
+                            else if(++flagPlus>2) throw new JsonSyntaxException("notplus");
+                            bild.append(cnPs);
+                            cnPs=cnNs;
+                            continue;
+                        }
+                        else throw new JsonSyntaxException("not+-");
+                    }
+                    if (isE(cnPs)) {
+                        if (isDigit(cnNs) || isOp(cnNs)) {
+                            if(++flagE>1) throw new JsonSyntaxException("notE");
+                            bild.append(cnPs);
+                            cnPs=cnNs;
+                            continue;
+                        }
+                        else throw new JsonSyntaxException("noE");
+                    }
+                    if (cnPs=='.'){
+                        if (isDigit(cnNs)) {
+                            if(++flagDot>1) throw new JsonSyntaxException("notdot");
+                            bild.append(cnPs);
+                            cnPs=cnNs;
+                            continue;
+                        }
+                        else throw new JsonSyntaxException("notdot");
+                    }
+                    if (isDigit(cnPs)){
+                        bild.append(cnPs);
+                        cnPs=cnNs;
+                        continue;
+                    }
                  }
-                 else throw new JsonSyntaxException("noE");
-            }
-            if (cnPs=='.')
-            {
-                if (isDigit(cnNs)) 
-                {
-                    if(++flagDot>1) throw new JsonSyntaxException("notdot");
-                    bild.append(cnPs);
-                    cnPs=cnNs;
-                    continue;
-                }
-                else throw new JsonSyntaxException("notdot");
-             }
-             if (isDigit(cnPs))
-             {
-                  bild.append(cnPs);
-                  cnPs=cnNs;
-                  continue;
-              }
-             else
-             {
-            	 if (isDigit(cnNs) || cnNs=='.')
-            	 {
-            		 bild.append(cnNs);
-            		 break;
-            	 }
-            	 else throw new JsonSyntaxException("end bad");
-             }
+            catch (JsonSyntaxException e) { e.printStackTrace(); throw new JsonSyntaxException("number not legal"); }
         }
+
+        else{
+            if (isDigit(cnNs) || cnNs=='.'){
+                bild.append(cnNs);
+                break;
+            }
+        else throw new JsonSyntaxException("end bad");}
+
     }
     Number temp;
-    if ((flagE+flagDot) != 0) temp = Double.parseDouble(bild.toString());
-    else temp = Integer.parseInt(bild.toString());
+    if ((flagE+flagDot) != 0) { temp = Double.parseDouble(bild.toString()); }
+    else { temp = Integer.parseInt(bild.toString()); }
 
     return new JsonNumber(temp);
 }
--- src/Json/Main.java
+++ src/Json/Main.java
@@ -8,17 +8,16 @@ public class Main {
     public static void main(String[] args) {
 
 		JsonBuilder k = null;
-		
-		try 
-		{
-			k = new JsonBuilder();
-			System.out.println(k.getV());
-		} 
-		catch (FileNotFoundException | JsonSyntaxException e)  
-		{
+		if (args.length!=1){
+			System.out.println("Program requires one input parameter : <file name/full path>");
+			System.exit(1);
+		}
+		try {
+			k = new JsonBuilder(args[0]);
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-    	
+
+    	System.out.println(k.getV());
     }
 }
